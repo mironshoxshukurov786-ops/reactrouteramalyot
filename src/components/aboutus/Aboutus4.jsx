78a6174b9dcfc971b70ccde5735c4e002622ecb5
start <mironshukurov786@gmail.com>
2026-01-31 18:09:12 +0500

    createx

--- src/components/aboutus/Aboutus4.jsx
+++ src/components/aboutus/Aboutus4.jsx
@@ -3,21 +3,12 @@ import React from 'react'
 export default function Aboutus4() {
   return (
     <div></div>
-  )
+  ) 
 }
 
 
 
 
-{/* <div className="">
-<p className="">bilmayman nimadir yozaman nimadirda karochi</p>  
-
-<h3 className="">nimadir</h3>
-
-<p className="">bilmayman nimadir yozaman nimadirda karochi</p>
-<p className="">bilmayman nimadir yozaman nimadirda karochi</p>
-<span>nimadir</span>
-</div>   */}
 
 
 
